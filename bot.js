195dbf9a4b0f206f623dbc1703388b651781a73c
SasaIs666 <49458334+SasaIs666@users.noreply.github.com>
2019-04-11 16:12:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,5 +40,13 @@ if (message.content.startsWith(adminprefix + 'setava')) {
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-.listen(process.env.PORT || 5000)
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
