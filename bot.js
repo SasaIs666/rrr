3dfa837559829aec3f9c5dc95b5ea3226dcf23a6
SasaIs666 <49458334+SasaIs666@users.noreply.github.com>
2019-06-11 19:18:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["396467906059173888","Id"]
+const developers = ["572089321411313664","Id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
